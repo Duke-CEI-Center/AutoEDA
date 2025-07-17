5bcc331be50a754de3adced117c3035c6937459e
AndyLu666 <yl996@duke.edu>
2025-07-17 15:00:44 -0400

    Add files via upload

---
+++ experiment/results/baseline2/case_0_generated.tcl
@@ -0,0 +1,35 @@
+# Floorplan TCL code for des design using Cadence Innovus
+
+create_floorplan -design des -top_module des3 -technology FreePDK45 -impl_version cpV1 -syn_version cpV1_clkP1_drcV1 -grid_index 0 -process_index 0
+
+set_floorplan_options -design des -core_utilization 0.7 -die_utilization 0.8 -core_density 0.7 -die_density 0.8
+
+set_floorplan_options -design des -core_utilization 0.7 -die_utilization 0.8 -core_density 0.7 -die_density 0.8
+
+set_floorplan_options -design des -core_utilization 0.7 -die_utilization 0.8 -core_density 0.7 -die_density 0.8
+
+set_floorplan_options -design des -core_utilization 0.7 -die_utilization 0.8 -core_density 0.7 -die_density 0.8
+
+set_floorplan_options -design des -core_utilization 0.7 -die_utilization 0.8 -core_density 0.7 -die_density 0.8
+
+set_floorplan_options -design des -core_utilization 0.7 -die_utilization 0.8 -core_density 0.7 -die_density 0.8
+
+set_floorplan_options -design des -core_utilization 0.7 -die_utilization 0.8 -core_density 0.7 -die_density 0.8
+
+set_floorplan_options -design des -core_utilization 0.7 -die_utilization 0.8 -core_density 0.7 -die_density 0.8
+
+set_floorplan_options -design des -core_utilization 0.7 -die_utilization 0.8 -core_density 0.7 -die_density 0.8
+
+set_floorplan_options -design des -core_utilization 0.7 -die_utilization 0.8 -core_density 0.7 -die_density 0.8
+
+set_floorplan_options -design des -core_utilization 0.7 -die_utilization 0.8 -core_density 0.7 -die_density 0.8
+
+set_floorplan_options -design des -core_utilization 0.7 -die_utilization 0.8 -core_density 0.7 -die_density 0.8
+
+set_floorplan_options -design des -core_utilization 0.7 -die_utilization 0.8 -core_density 0.7 -die_density 0.8
+
+set_floorplan_options -design des -core_utilization 0.7 -die_utilization 0.8 -core_density 0.7 -die_density 0.8
+
+set_floorplan_options -design des -core_utilization 0.7 -die_utilization 0.8 -core_density 0.7 -die_density 0.8
+
+commit_floorplan -design des
