64ce56c36cfae94e2ddcf92d370a8c4f17cb9a01
JJJayyyy <jzzjy0822@gmail.com>
2025-07-30 11:17:40 -0700

    Delete scripts/FreePDK45/backend/8_save_design.tcl

--- scripts/FreePDK45/backend/8_save_design.tcl
+++
@@ -1,9 +0,0 @@
-# Generate File
-# set top_module $::env(TOP_NAME)
-write_lef_abstract pnr_out/${TOP_NAME}_pnr.lef
-write_lef_library pnr_out/${TOP_NAME}_lib.lef
-#do_extract_model -view default pnr_out/${TOP_NAME}_pnr.lib
-saveNetlist pnr_out/${TOP_NAME}_pnr.v
-streamOut pnr_out/${TOP_NAME}_pnr.gds.gz
-
-exec touch _Finished_
